c1373e0d77607e3a1d337278b4edbb42af5cb144
neoe <neoedmund@gmail.com>
2019-05-16 13:09:00 +0800

    add feature: scan main class in path

--- src/neoe/neoejloader/Loader.java
+++ src/neoe/neoejloader/Loader.java
@@ -1,49 +1,113 @@
 package neoe.neoejloader;
 
 import java.io.File;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.*;
-
-
-import java.text.*;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 public class Loader {
 
-    /**
-     * @param args
-     * @throws Exception
-     */
-    public static void main(String[] args) throws Exception {
-        System.out.println("param: lib-dir main-class params...");
-        String libDir = args[0];
-        String mainClass = args[1];
-        String[] args2 = new String[args.length - 2];
-        if (args2.length > 0)
-            System.arraycopy(args, 2, args2, 0, args2.length);
-        load(libDir, mainClass, args2);
-    }
+	/**
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		System.out.println("param: lib-dir main-class params...");
+		String libDir = args[0];
+		if (args.length == 1) {
+			System.out.println("main-class is not paramed, so scan lib-dir");
+			load(libDir, null, null);
+			return;
+		}
+		String mainClass = args[1];
+		String[] args2 = new String[args.length - 2];
+		if (args2.length > 0)
+			System.arraycopy(args, 2, args2, 0, args2.length);
+		load(libDir, mainClass, args2);
+	}
 
-    public static void load(String libDir, String mainClass, String[] args)
-            throws Exception {
-        Iterable<File> it = new FileIterator(libDir);
-        List<URL> jars = new ArrayList<URL>();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-        for (File f : it) {
-            if (f.isDirectory()) {
-                continue;
-            }
-            if (f.getName().toLowerCase().endsWith(".jar")) {
-                System.out.println("add " + f.getAbsolutePath()+" \t "+sdf.format(new Date(f.lastModified())));
-                jars.add(f.toURI().toURL());
-            }
-        }
-        System.out.println("jar cnt=" + jars.size() + " run " + mainClass + " "
-                + Arrays.deepToString(args));
-        new URLClassLoader(jars.toArray(new URL[jars.size()]), Loader.class
-                .getClassLoader()).loadClass(mainClass).getMethod("main",
-                new Class[] { String[].class }).invoke(null,
-                new Object[] { args });
-    }
+	public static void load(String libDir, String mainClass, String[] args) throws Exception {
+		Iterable<File> it = new FileIterator(libDir);
+		List<URL> jars = new ArrayList<URL>();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		for (File f : it) {
+			if (f.isDirectory()) {
+				continue;
+			}
+			if (f.getName().toLowerCase().endsWith(".jar")) {
+				System.out.println("add " + f.getAbsolutePath() + " \t " + sdf.format(new Date(f.lastModified())));
+				jars.add(f.toURI().toURL());
+			}
+		}
+		URLClassLoader cl = new URLClassLoader(jars.toArray(new URL[jars.size()]), Loader.class.getClassLoader());
+		if (mainClass == null) {
+			List mains = new ArrayList();
+			scan(cl, jars, mains);
+			System.out.println(String.format("Found %d main", mains.size()));
+		} else {
+			System.out.println("jar cnt=" + jars.size() + " run " + mainClass + " " + Arrays.deepToString(args));
+			cl.loadClass(mainClass).getMethod("main", new Class[] { String[].class }).invoke(null,
+					new Object[] { args });
+		}
+	}
+
+	private static void scan(URLClassLoader cl, List<URL> jars, List mains) throws Exception {
+		for (URL url : jars) {
+			scan(cl, url, mains);
+		}
+	}
+
+	private static void scan(URLClassLoader cl, URL url, List mains) throws Exception {
+		File f = new File(url.toURI().getPath());
+		ZipFile zip = new ZipFile(f);
+		List res = new ArrayList();
+		try {
+			for (Enumeration list = zip.entries(); list.hasMoreElements();) {
+				ZipEntry entry = (ZipEntry) list.nextElement();
+				String fn = entry.getName();
+				if (fn.endsWith(".class")) {
+					String cn = fn.substring(0, fn.length() - 6).replace('/', '.');
+					if (scanClass(cl, cn)) {
+						res.add(cn);
+					}
+				}
+
+			}
+		} finally {
+			zip.close();
+		}
+		if (!res.isEmpty()) {
+			System.out.println("[in " + f.getName() + "]");
+			for (Object o : res) {
+				System.out.print("\t");
+				System.out.println(o);
+			}
+			if (mains != null)
+				mains.addAll(res);
+		}
+	}
+
+	private static boolean scanClass(URLClassLoader cl, String cn) {
+		try {
+			Class clz = cl.loadClass(cn);
+			Method method = clz.getMethod("main", new Class[] { String[].class });
+			int mods = method.getModifiers();
+			if (method.getReturnType().equals(Void.TYPE) && Modifier.isPublic(mods) && Modifier.isStatic(mods)) {
+				return true;
+			}
+		} catch (Throwable e) {
+		}
+		return false;
+
+	}
 
 }
